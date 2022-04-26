313a7f3e5ed8da9d60eff6d4b8a4b8a63e9d3d24
Nicole-Miguel <101760889+Nicole-Miguel@users.noreply.github.com>
2022-04-25 23:32:31 -0300

    alteracao da prop perimetro

--- Retangulo.cs
+++ Retangulo.cs
@@ -3,7 +3,7 @@ public class Retangulo
     public double Base { get; set; }
     public double Altura { get; set; }
     public double Area { get => Base * Altura; }
-    public double Perimetro { get => 2 * (Base * Altura); }
+    public double Perimetro { get => 2 * (Base + Altura); }
 
     public Retangulo(double _base, double altura)
     {
